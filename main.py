131828c904caaac18d88ca7e1f7137f8039a3c88
Jason Dunn <jaygeorgedunn@gmail.com>
2026-02-04 15:37:45 -0800

    ml demo ready

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:3000"],
+    # allow_origins=["http://localhost:3000"] ,
     allow_origins=["https://stateshaper-ml-demo.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
