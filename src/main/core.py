8b9e988534aed6484fa702f5c87551150a303a63
Jason Dunn <jaygeorgedunn@gmail.com>
2026-02-11 17:20:14 -0800

    new lite main branch

--- src/main/core.py
+++ src/main/core.py
@@ -1,44 +1,18 @@
-
-import sys
-
-
 class Stateshaper:
 
-    def __init__(self, state=5, vocab=[], constants={"a": 3, "b": 5, "c": 7, "d": 11}, mod=9973, compound=None):
+    def __init__(self, initial_state=1, constants={"a": 3, "b": 5, "c": 7, "d": 11}, mod=9973):
 
-        if isinstance(state, int) == False:
-            self.current_state = [int(x) % mod for x in state]
-            self.original_state = [int(x) % mod for x in state]
+        if isinstance(initial_state, int) == False:
+            self.current_state = [int(x) % mod for x in initial_state]
+            self.original_state = [int(x) % mod for x in initial_state]
         else:
-            self.current_state = state
-            self.original_state = state
+            self.current_state = [initial_state]
+            self.original_state = [initial_state]
     
-        self.current_vocab = vocab
-        self.original_vocab = vocab
         self.constants = constants
         self.mod = mod
-        try:
-            compound[0] = [compound[0]] if isinstance(compound[0], int) == True else compound[0] if compound else None
-        except:
-            pass
-        self.compound = compound
-        print(compound)
-        
-       
         self.iteration = 1 
-
         self.prior_index = 0
-
-        self.seed_format = {
-            "state": self.current_state,
-            "vocab": self.original_vocab,
-            "constants": self.constants,
-            "mod": self.mod
-        }
-
-        if compound:
-            self.seed_format["compound"] = compound
-
         self.token_array = []
 
 
@@ -47,7 +21,7 @@ class Stateshaper:
     def step(self): 
         self.morph_array()
         self.iteration += 1
-        return self.get_token() if not self.compound else self.compound_token()
+        return self.get_token() 
 
 
     def reverse(self):
@@ -57,20 +31,15 @@ class Stateshaper:
             self.current_state = self.current_state[:len(self.current_state)-1] 
             old_value = [self.reverse_morph(value)]
             self.current_state = old_value + self.current_state
-        return self.get_token() if not self.compound else self.compound_token()
+        return self.get_token() 
 
 
     def get_token(self):
-        return self.current_vocab[self.index_token()] if len(self.current_vocab) > 0 else self.current_state[0]
+        return self.current_state[0]
     
 
     def generate_tokens(self, amount):
-        self.rebuild()
         return [self.step() for _ in range(amount)]
-    
-
-    def index_token(self):
-        return self.current_state[0] * (self.constants["a"] * (self.constants["c"] + self.constants["b"])) % len(self.current_vocab)
 
 
     def morph_array(self):
@@ -92,32 +61,5 @@ class Stateshaper:
     
 
     def jump(self, index):
-        self.rebuild()
         tokens = [self.step() for _ in range(index)]
         return tokens[len(tokens)-1]
-
-
-    def rebuild(self): 
-        self.iteration = 1
-        self.current_state = self.original_state
-
-
-    def compound_token(self):
-        count = 0
-        tokens = []
-
-        current = self.compound[0][self.current_state[0] * (self.constants["c"] if self.current_state[0] % 3 == 0 else self.constants["b"]) % len(self.compound[0])]
-        group = self.current_vocab[(self.current_state[0] * self.constants["b"] if self.current_state[0] * self.current_state[0] * self.constants["d"] % 3 == 0 else self.current_state[0] * self.constants["d"]) % len(self.current_vocab)]
-        
-        while (len(tokens) < current and current < len(group)) or (len(tokens) < len(group) and current >= len(group)):
-            token = group[(self.current_state[0] + count + self.iteration * (len(tokens)*self.constants["c"]) * (self.constants["a"] * (self.constants["c"] + self.constants["b"]))) % len(group)] 
-            if token in tokens:
-                count += 1
-            else:
-                tokens.append(token) 
-        i = 1
-        while i < len(tokens):
-            tokens.insert(i, self.compound[2][((len(tokens) + 1) * self.iteration * self.constants["c"]) % len(self.compound[2])])
-            i += 2
-
-        return " ".join(tokens)
--- src/main/stateshaper.py
+++ src/main/stateshaper.py
@@ -1,110 +1,44 @@
-import json
-import random
-import sys
-from connector.Connector import Connector
 from core import Stateshaper
-from tools.derive_vocab.DeriveVocab import DeriveVocab
-from tools.tiny_state.TinyState import TinyState
-from example_data.format_data.FormatData import FormatData
-
 
 
 class RunEngine:
 
-    def __init__(self, data=None, original_data=None, seed=None, token_count=10, initial_state=5, vocab=None, constants={"a": 3,"b": 5,"c": 7,"d": 11}, mod=9973, **kwargs):
-        
-
-        if isinstance(data, dict):
-            self.data = data
-        else:
-            print("Data is not formatted or formatted incorrectly. See accepted data formats in the 'example_data' directory.")
-            sys.exit()
-
-        self.original_data = original_data
-        
-        self.seed = None
-
-        self.first_run = True if not seed else False
-
-        self.token_count = token_count
-
-
-        if isinstance(initial_state, int):
-            initial_state = [initial_state]
-
-
-        if isinstance(seed, dict):
-            try:
-                initial_state = seed["s"] 
-            except:
-                initial_state = initial_state 
-            try:
-                vocab = seed["v"]
-            except:
-                vocab = vocab
-            try:
-                constants = seed["c"]
-            except:
-                constants = constants
-            try: 
-                mod = seed["m"] 
-            except: 
-                mod = mod
-        
-            self.seed = {
-                "state": initial_state,
-                "vocab": vocab,
-                "constants": constants,
-                "mod": mod
-            }
-
-        self.connector = Connector(data=self.data, token_count=token_count, initial_state=initial_state,  constants=constants, vocab=vocab, mod=mod)
-
-        self.tiny_state = TinyState()
-
-        self.format_data = FormatData()
-
-        self.derive_vocab = DeriveVocab()
-
-        self.engine = None
-
-        self.default_seed = None
-
-        self.derived_seed = None
+    def __init__(self, initial_state=1, constants={"a": 3,"b": 5,"c": 7,"d": 11}, mod=9973, **kwargs):
+    
+        self.initial_state = initial_state
+        self.constants = constants
+        self.mod = mod
 
 
 
 
     def start_engine(self):
         self.engine = None
-        self.seed = self.connector.start_connect()
         self.define_engine()
 
 
-    def define_engine(self, state=None, vocab=None, constants=None, mod=None):
+    def define_engine(self, initial_state=None, constants=None, mod=None):
         self.engine = Stateshaper(
-            self.seed["state"] if not state else state,
-            self.seed["vocab"] if not vocab else vocab,
-            self.seed["constants"] if not constants else constants,
-            self.seed["mod"] if not mod else mod,
-            [self.data["compound_length"], self.data["compound_groups"], self.data["compound_terms"]] if self.data["rules"] == "compound" else None
+            initial_state = self.initial_state if not initial_state else initial_state,
+            constants = self.constants if not constants else constants,
+            mod = self.mod if not mod else mod
         )
 
 
-    def run_engine(self, token_count=None):
-        self.tokens = self.engine.generate_tokens(self.token_count if not token_count else token_count)
+    def run_engine(self, token_count=10):
+        self.tokens = self.engine.generate_tokens(token_count)
         print("\n\nTokens successfully generated from vocab.")
         print("============================================")
         print(self.tokens)
-
         return self.tokens
     
 
-    def reverse(self, token_count=None):
-        self.tokens = [self.engine.reverse() for _ in range(self.token_count if not token_count else token_count)]
+    def reverse(self, token_count=10):
+        self.tokens = [self.engine.reverse() for _ in range(token_count)]
         print("\n\nToken re-created from reverse.")
         print("============================================")
         print(self.tokens)
+        return self.tokens
 
     
     def jump(self, index):
@@ -112,6 +46,7 @@ class RunEngine:
         print(f"\n\nToken successfully accessed from index {str(index)}.")
         print("============================================")
         print(self.tokens)
+        return self.tokens
 
 
     def one_token(self):
@@ -128,37 +63,3 @@ class RunEngine:
         print("============================================")
         print(self.tokens)
         return self.tokens
-
-
-    def rebuild(self):
-        self.engine.rebuild()
-
-
-    def get_seed(self, state=None, vocab=False, constants=None, mod=None):
-        return self.connector.output_seed(state=state, vocab=vocab, constants=constants, mod=mod)
-    
-    
-    def get_derived(self):
-        return self.derived_seed
-    
-
-    def adjust_ratings(self):
-        for _ in range(20):
-            self.derive_vocab.adjust_rankings(self.original_data, self.data)
-        new_vocab = self.derive_vocab.current_vocab()
-        self.seed["vocab"]  = new_vocab
-        self.derived_seed = self.new_seed(new_vocab)
-        self.define_engine()
-
-
-    def new_seed(self, vocab):
-        seed = {}
-
-        seed["s"] = self.seed["state"]  
-        seed["v"] = self.connector.compress_regular(vocab)
-        seed["c"] = self.seed["constants"]
-        seed["m"] = self.seed["mod"]
-
-        self.derived_seed = seed["v"]  
-
-        return seed
